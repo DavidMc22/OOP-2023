bb682e692d3468b411ab3db75460fb8fc2ce18a7
David McCormack <David.mccormack07@gmail.com>
2023-01-30 12:54:06 +0000

    Triangle lession

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -1,5 +1,7 @@
 package ie.tudublin;
 
+import javax.swing.border.StrokeBorder;
+
 import processing.core.PApplet;
 
 public class HelloProcessing extends PApplet
@@ -8,11 +10,12 @@ public class HelloProcessing extends PApplet
 	public void settings()
 	{
 		size(500, 500);
+		
 	}
 
 	public void setup() {
 		
-
+		background(255,0,0);
 	
 		
 	}
@@ -20,23 +23,16 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-
-		stroke(225);
-		line(10,10,100,100); //x1 y1 x2 y2
-		circle(300,250,70); //CX, CY, d  
-		fill(0,255,0);
-		rect(10,300,10,100); // TLX, TLY, W, H
-		stroke(127); //outines shapes 
-		fill(0,0,255);// Fills shapes 
-		noStroke(); //
-		noFill();// Takes away the fill
-		strokeWeight(1); // Changes tickness of lines
-		triangle(40,90,300,20,80,70);
-		fill(0,0,255);
-
-
-
-
+		fill(255,255,0);
+		circle(250,250,400);
+		fill(0,255,255);
+		triangle(250, 0, 40, 400, 460, 400);
+		fill(255,255,255);
+		ellipse(250,200,200,120);
+		fill(0,0,0);
+		circle(250,200,90);
+		
+		
 
 
 
