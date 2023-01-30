8bbd2572f0c6d8872eceec9b099ae217f77a5882
David McCormack <David.mccormack07@gmail.com>
2023-01-30 12:20:33 +0000

    How to draw lession 1

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -20,6 +20,25 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		
+
+		stroke(225);
+		line(10,10,100,100); //x1 y1 x2 y2
+		circle(300,250,70); //CX, CY, d  
+		fill(0,255,0);
+		rect(10,300,10,100); // TLX, TLY, W, H
+		stroke(127); //outines shapes 
+		fill(0,0,255);// Fills shapes 
+		noStroke(); //
+		noFill();// Takes away the fill
+		strokeWeight(1); // Changes tickness of lines
+		triangle(40,90,300,20,80,70);
+		fill(0,0,255);
+
+
+
+
+
+
+
 	}
 }
